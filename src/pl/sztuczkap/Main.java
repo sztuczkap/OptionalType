91b94a7b554cd2358c126cf780683569de1237a0
sztuczkap <p.sztuczka@wp.pl>
2020-12-30 19:50:31 +0100

    przetwarzanie typu Optional

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,12 +1,14 @@
 package pl.sztuczkap;
 
 import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // przyklad z bledem NullPointerException
+/*        // przyklad z bledem NullPointerException
         String text = null;
         if (text != null) {  // standardowe sprawdzenie przy javie8
             System.out.println(text.length());
@@ -22,7 +24,65 @@ public class Main {
         // przyklad w petli
         if (optional.isPresent()) {
             System.out.println(optional.get());
-        }
+        }*/
+
+        // ==========================================================
+
+        String text = "Hello";
+        String text2 = null;
+
+        Optional<String> optional = Optional.of(text);
+        optional.ifPresent(System.out::println);
+
+        Optional<String> optional2 = Optional.ofNullable(text2);
+
+        System.out.println(optional2.orElse("Hi"));  // przydatna do domyslnych wartosci
+
+        System.out.println(optional2.orElseGet(() -> "Hi2")); // przekazujemy suppliera (roznica jest opoznione wywolanie)
+
+        System.out.println(optional2.orElse(defaultValue()));
+
+        System.out.println(optional2.orElseGet(defaultValue2()));
+
+        //=============================
+
+        // Supplier przyjmuje typ Optional
+        Optional<String> optional3 = optional2.or(() -> Optional.of("HIII!"));
+
+//        // tworzymy zmienna word do ktorej chcemy przypisac wartosc optionala
+//        // chyba ze ten nie istnieje to  wrzucimy wartosc domyslana...
+//        String word = optional2.orElseThrow(IllegalArgumentException::new);
+//        System.out.println(word);
+//
+
+        optional2.ifPresentOrElse(success(), failure()); // zalążek programowania reaktywnego
+
+
+        //=============================
 
     }
+
+    // testujemy tworzac nowa metode
+    private static String defaultValue() {
+        System.out.println("Wywołanie");
+        return "Hi!";
+    }
+
+    // przy orElseGet(defaultValue2())
+    private static Supplier<String> defaultValue2() {
+        return () -> {
+            System.out.println("Wywołanie");
+            return "Hi! Supplier";
+        };
+
+    }
+
+    private static Consumer<String> success(){
+        return text -> System.out.println(text);
+    }
+
+    private static Runnable failure(){
+        return () -> System.out.println("optional is empty");
+    }
+
 }
