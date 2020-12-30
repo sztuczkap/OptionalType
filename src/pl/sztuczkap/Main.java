9973dafe73fcdfe7f256d87b058efe36c2d7fbf5
sztuczkap <p.sztuczka@wp.pl>
2020-12-30 18:37:39 +0100

    typ Optional

--- src/pl/sztuczkap/Main.java
+++ src/pl/sztuczkap/Main.java
@@ -1,8 +1,28 @@
 package pl.sztuczkap;
 
+import java.util.Optional;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // przyklad z bledem NullPointerException
+        String text = null;
+        if (text != null) {  // standardowe sprawdzenie przy javie8
+            System.out.println(text.length());
+        }
+
+
+        Optional<String> empty = Optional.empty();
+//        Optional<String> optional = Optional.of(text);  // NullPointer Exception
+        Optional<String> optional = Optional.ofNullable(text);
+        System.out.println(optional.isPresent());
+        System.out.println(optional.isEmpty());  // od javy 11
+
+        // przyklad w petli
+        if (optional.isPresent()) {
+            System.out.println(optional.get());
+        }
+
     }
 }
